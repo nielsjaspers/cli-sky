31d61562fc767da2c5c105b012ebf5177dc18e4f
Niels Jaspers <naeliss.github@gmail.com>
2025-02-15 14:04:13 +0100

    feat(cli-arguments): refreshing sessions
    you can now refresh sessions using the cli

--- cmd/refresh.go
+++ cmd/refresh.go
@@ -10,32 +10,28 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var handlePost string
-
-var postCmd = &cobra.Command{
-	Use:   "post \"message\"",
-	Short: "Post a message to Bluesky",
-	Long:  "Allows you to post a message to Bluesky with an optional handle.",
+var refreshCmd = &cobra.Command{
+	Use:   "refresh \"@handle.bsky.com\"",
+	Short: "Refresh a session",
+	Long:  "Allows you to refresh a session.",
 	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		message := args[0]
+		handle := args[0]
 
 		// Clean the handle (remove "@" if present)
-		cleanedHandle := strings.TrimPrefix(handlePost, "@")
+		cleanedHandle := strings.TrimPrefix(handle, "@")
 
         authData, err := datahandler.ReadAuthResponseFromFile(cleanedHandle)
         if err != nil {
             log.Fatalf("Error reading Auth data: %v", err)
         }
 
-		fmt.Printf("Posting message: %s\n", message)
 		fmt.Printf("Using handle: %s\n", cleanedHandle)
-        bluesky.Post(message, authData)
+        bluesky.RefreshSession(authData.RefreshJwt)
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(postCmd)
-	postCmd.Flags().StringVarP(&handlePost, "handle", "u", "", "Bluesky handle (optional, @handle.bsky.social)")
+	rootCmd.AddCommand(refreshCmd)
 }
 
